eab61d93793670d86113c3bd016c539a6103c0be
ivan-lazarev1 <lazarev_vanya01@mail.ru>
2020-08-06 14:11:28 +0400

    create: exchange google map

--- js/main.js
+++ js/main.js
@@ -14,15 +14,6 @@ $(document).ready(function () {
       onlyInViewport: false,
     },
   });
-
-  ymaps.ready(init);
-  function init() {
-    var myMap = new ymaps.Map("map", {
-      center: [7.89068942, 98.29499271],
-      zoom: 13,
-    });
-  }
-
   var reviewsSlider = new Swiper(".reviews-slider", {
     // Optional parameters
     loop: true,
